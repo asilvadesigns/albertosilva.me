78edcbf34d07aa5cb8c8a828489e9f042d37f156
bethanydoula <bethanydoula27@gmail.com>
2016-08-17 22:22:13 -0400

    init v3

---
+++ app/_posts/2016-07-24-hi-im-bethany.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title:  "Hi I'm Bethany"
+date:   2016-07-24 12:35:39 -0400
+categories: intro blog
+---
+## this is a test
--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,66 @@
-var gulp         = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var browsersync  = require('browser-sync');
-var cp			 = require('child_process');
-var concat       = require('gulp-concat');
-var cssnano      = require('gulp-cssnano');
-var sass         = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
-var uglify       = require('gulp-uglify');
-var util		 = require('gulp-util');
+var browsersync = require('browser-sync');
+var cp          = require('child_process');
+var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+var sass        = require('gulp-sass');
 
-var basePath = {
-	src: './',
-	dist: '_site/'
-}
+var paths = {
+    html: {
+        src: './app/**/*.html',
+    },
+    jekyll: {
+        src: './app',
+        dist: './_site',
+    },
+    markdown: {
+        src: './app/**/*.markdown',
+    },
+    styles: {
+        src: './app/_sass/**/*.scss',
+        dist: './app/css',
+    }
+};
 
-var assetsPath = {
-	html: {
-		src: basePath.src + '/**/*.html'
-	},
-	markdown: {
-		src: basePath.src + '/**/*.md'
-	},
-	jekyll: {
-		src: basePath.dist
-	},
-	scripts: {
-		src: basePath.src + '/_js/**/*.js',
-		dist: basePath.src + '/js/'
-	},
-	styles: {
-		src: basePath.src + '/_sass/**/*.scss',
-		dist: basePath.src + '/css/'
-	}
-}
-
-gulp.task(build);
+gulp.task(jekyllbuild);
 gulp.task(reload);
-gulp.task(scripts);
 gulp.task(serve);
 gulp.task(styles);
 gulp.task(watch);
 gulp.task('default',
-	gulp.series(
-		'styles',
-		'scripts',
-		'build',
-		gulp.parallel('serve', 'watch')
-	)
-)
+    gulp.series(
+        styles,
+        jekyllbuild,
+        gulp.parallel(serve, watch))
+);
 
-function scripts() {
-	return gulp.src(assetsPath.scripts.src)
-		.pipe(gulp.dest(assetsPath.scripts.dist))
-		.pipe(browsersync.stream());
+function jekyllbuild(done) {
+    browsersync.notify('building jekyll');
+    return cp.spawn('jekyll.bat',  ['build'], {stdio: 'inherit'})
+        .on('close', done);
 }
 
-function styles() {
-	return gulp.src(assetsPath.styles.src)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(assetsPath.styles.dist))
-		.pipe(browsersync.stream());
-}
-
-function build() {
-	return cp.spawn('jekyll.bat', ['build'], {studio: 'inherit'});
-}
-
-function reload() {
-	browsersync.reload();
+function reload(cb) {
+    browsersync.reload();
+    cb();
 }
 
 function serve() {
-    browsersync.init({server: assetsPath.jekyll.src});
+    browsersync.init({
+        server: paths.jekyll.dist,
+        notify: true
+    })
+}
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        //  .pipe(sass().on('error', sass.logError))
+        .pipe(sass())
+        .pipe(gulp.dest(paths.jekyll.dist + '/css'))
+        .pipe(gulp.dest(paths.styles.dist))
 }
 
 function watch() {
-	gulp.watch(assetsPath.html.src).on('change', reload);
-	gulp.watch(assetsPath.markdown.src).on('change', reload);
-	gulp.watch(assetsPath.scripts.src).on('change', scripts);
-	gulp.watch(assetsPath.styles.src).on('change', styles);
+    gulp.watch(paths.styles.src).on('change', gulp.series(styles, reload));
+    gulp.watch(paths.html.src).on('change', gulp.series(jekyllbuild, reload));
+    gulp.watch(paths.markdown.src).on('change', gulp.series(jekyllbuild, reload));
 }
